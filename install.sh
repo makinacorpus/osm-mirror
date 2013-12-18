f9bfb624a3357b1540c346ab5bbefe984df7e2e8
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-12-18 15:38:08 +0100

    First set of fixes after successfull deployment

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ apt-get dist-upgrade -y
 
 echo_step "Install necessary components..."
 
-apt-get install -y git wget libgdal1 gdal-bin mapnik-utils unzip
+apt-get install -y git curl wget libgdal1 gdal-bin mapnik-utils unzip
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y libapache2-mod-tile
@@ -40,10 +40,10 @@ apt-get install -y libapache2-mod-tile
 
 if [ ! -f README.md ]; then
    echo_step "Downloading installer source..."
-   git clone --depth=50 --branch=master https://github.com/makinacorpus/osm-mirror.git /tmp/osm-mirror
+   git clone --recursive --depth=50 --branch=master https://github.com/makinacorpus/osm-mirror.git /tmp/osm-mirror
    rm -f /tmp/osm-mirror/install.sh
    shopt -s dotglob nullglob
-   mv /tmp/osm-mirror/* .
+   cp -R /tmp/osm-mirror/* .
 fi
 
 
@@ -69,11 +69,11 @@ source $OSM_MIRROR_CONF
 
 echo_step "Configure database..."
 
-sudo -n -u postgres -s -- psql -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy.sql
 sudo -n -u postgres -s -- psql -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';"
 sudo -n -u postgres -s -- psql -c "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER} ENCODING 'UTF8' TEMPLATE template0;"
 sudo -n -u postgres -s -- psql -d ${DB_NAME} -c "CREATE EXTENSION postgis;"
 sudo -n -u postgres -s -- psql -d ${DB_NAME} -c "GRANT ALL ON spatial_ref_sys, geometry_columns, raster_columns TO ${DB_USER};"
+sudo -n -u postgres -s -- psql -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy.sql
 
 cat << _EOF_ >> /etc/postgresql/9.1/main/pg_hba.conf
 # Automatically added by OSM installation :
@@ -94,26 +94,25 @@ ogr2ogr $OSM_DATA/ne_10m_populated_places_fixed.shp $OSM_DATA/ne_10m_populated_p
 zipfile=/tmp/simplified-land-polygons-complete-3857.zip
 curl -L -o "$zipfile" "http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip"
 unzip -qqu $zipfile simplified-land-polygons-complete-3857/simplified_land_polygons.{shp,shx,prj,dbf,cpg} -d /tmp
+rm $zipfile
 mv /tmp/simplified-land-polygons-complete-3857/simplified_land_polygons.* $OSM_DATA/
 
 zipfile=/tmp/land-polygons-split-3857.zip
 curl -L -o "$zipfile" "http://data.openstreetmapdata.com/land-polygons-split-3857.zip"
 unzip -qqu $zipfile -d /tmp
+rm $zipfile
 mv /tmp/land-polygons-split-3857/land_polygons.* $OSM_DATA/
 
 zipfile=/tmp/coastline-good.zip
 curl -L -o "$zipfile" "http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip"
 unzip -qqu $zipfile -d /tmp
+rm $zipfile
 mv /tmp/coastline-good.* $OSM_DATA/
-
-zipfile=/tmp/shoreline_300.zip
-curl -L -o "$zipfile" "http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip"
-unzip -qqu $zipfile -d /tmp
-mv /tmp/shoreline_300.* $OSM_DATA/
 
 zipfile=/tmp/10m-land.zip
 curl -L -o "$zipfile" "http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip"
 unzip -qqu $zipfile -d /tmp
+rm $zipfile
 mv /tmp/10m-land.* $OSM_DATA/
 
 shapeindex --shape_files \
@@ -134,6 +133,9 @@ echo_step "Load OpenStreetMap data..."
 croncmd="`pwd`/update.sh 2> /var/log/openstreetmap-errors"
 cronjob="0 2 1 * * $croncmd"
 ( crontab -u root -l 2> /dev/null | grep -v "$croncmd" ; echo "$cronjob" ) | crontab -u root -
+
+/usr/bin/install-postgis-osm-user.sh ${DB_NAME} www-data
+/usr/bin/install-postgis-osm-user.sh ${DB_NAME} gisuser
 
 
 #.......................................................................
@@ -159,16 +161,16 @@ font_dir_recurse=false
 [osm]
 URI=/osm/
 XML=/etc/mapnik-osm-data/osm.xml
-DESCRIPTION=This is the standard osm mapnik style
+DESCRIPTION=OSM
 
 [grayscale]
 URI=/grayscale/
 XML=/etc/mapnik-osm-data/makina/grayscale/grayscale.xml
 DESCRIPTION=Grayscale
 
-[bright]
-URI=/bright/
-XML=/etc/mapnik-osm-data/makina/bright/bright.xml
+[osmbright]
+URI=/osmbright/
+XML=/etc/mapnik-osm-data/makina/osmbright/osmbright.xml
 DESCRIPTION=Bright
 _EOF_
 
@@ -177,27 +179,32 @@ _EOF_
 
 echo_step "Deploy preview map..."
 
-cp -R preview/* /var/www/osm
+rm /var/www/index.html
+cp -R preview/* /var/www/
 
-
-cat << _EOF_ > /var/www/osm/conf.js
+cat << _EOF_ > /var/www/conf.js
 var SETTINGS = {
     extent: [${EXTENT}],
     layers: {
         'OSM': '/osm/{z}/{x}/{y}.png',
         'Grayscale': '/grayscale/{z}/{x}/{y}.png',
-        'Bright': '/bright/{z}/{x}/{y}.png'
+        'OSM Bright': '/osmbright/{z}/{x}/{y}.png'
     }
 };
+
 _EOF_
 
 
+#.......................................................................
 
 echo_step "Restart services..."
 
 /etc/init.d/postgresql restart
-/etc/init.d/renderd restart
 /etc/init.d/apache2 restart
+/etc/init.d/renderd restart
 
+
+#.......................................................................
 
 echo_step "Done."
+
--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ OSM_MIRROR_CONF=/etc/default/openstreetmap-conf
 source $OSM_MIRROR_CONF
 
 output=/tmp/data.osm
-wget http://www.overpass-api.de/api/xapi?map?bbox=${extent} -O ${output}
+wget http://www.overpass-api.de/api/xapi?map?bbox=${EXTENT} -O ${output}
 sudo -n -u postgres -s -- osm2pgsql -d ${DB_NAME} --extra-attributes ${output}
 rm ${output}

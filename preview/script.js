7858a3593c6419a3cd777c6443884397a1a38b8c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-12-18 15:37:20 +0100

    Fix initialization of layercontrol minimap

---
+++ preview/script.js
@@ -0,0 +1,21 @@
+var bounds = L.latLngBounds([SETTINGS.extent[1], SETTINGS.extent[0]],
+                            [SETTINGS.extent[3], SETTINGS.extent[2]]);
+
+var map = L.map('map', {maxBounds: bounds});
+
+var options = {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+};
+
+var baselayers = {};
+for (var k in SETTINGS.layers) {
+    baselayers[k] = L.tileLayer(SETTINGS.layers[k], options);
+}
+baselayers['OSM'].addTo(map);
+
+L.control.layers.minimap(baselayers, [], {
+    position: 'bottomright',
+    collapsed: false
+}).addTo(map);
+
+map.fitBounds(bounds);

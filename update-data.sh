7d915fb945cced90693c8cae1589c934ef845592
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-12-20 16:53:35 +0100

    WIP render list

--- update-data.sh
+++ update-data.sh
@@ -3,6 +3,8 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 OSM_MIRROR_CONF=/etc/default/openstreetmap-conf
 
+STYLES_PATH=/etc/mapnik-osm-data/makina
+
 source $OSM_MIRROR_CONF
 
 function echo_step () {
@@ -26,8 +28,13 @@ fi
 
 echo_step "Load into database..."
 
-sudo -n -u postgres -s -- osm2pgsql -d ${DB_NAME} --extra-attributes --create ${output}
+sudo -n -u postgres -s -- osm2pgsql -d ${DB_NAME} --extra-attributes --create --expire-tiles 9-14 -o /tmp/expire.list ${output}
 if [ $? -eq 0 ]; then
     rm ${output}
+
+    for style in `ls $STYLES_PATH`; do
+        cat /tmp/expire.list | awk '{split($0,a,"/"); print a[2],a[3],a[1]}' | render_list --map=$style --socket=/var/run/renderd/renderd.sock
+    done;
+
     echo_step "Done."
 fi

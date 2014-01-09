1a5a9f6980b7dbb8b2b75ca48245819f1e6b151f
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-09 10:45:15 +0100

    Render map according to specified scale and extent

--- render-raster.sh
+++ render-raster.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
+import math
+import re
 import logging
+import optparse
 
 try:
     from mapnik import *
@@ -8,32 +11,71 @@ except ImportError:
     from mapnik2 import *
 
 
+OSM_MIRROR_CONF = '/etc/default/openstreetmap-conf'
+
+
+"""
+
+    Configuration
+
+    Read from command-line arguments, with defaults coming from OSM_MIRROR_CONF
+
+"""
+
+def readconf():
+    config = open(OSM_MIRROR_CONF).readlines()
+    config = [line.strip().replace('"', '') for line in config]
+    return dict([re.split(r'\s*=\s*', line) for line in config])
+
+
+config = readconf()
+
+parser = optparse.OptionParser(usage="""%prog <stylename> <filename> [options]""")
+parser.add_option('-e', '--extent', dest='extent', nargs=4,
+                  help='Geographical bounding box. Two long lat pairs e.g. 2.04 43.88 2.22 43.98 (Albi)',
+                  type='float',
+                  default=[float(v) for v in config['EXTENT'].split(',')],
+                  action='store')
+parser.add_option('-s', '--scale', dest='scale',
+                  help='Scale as integer (e.g. 25000 for 1:25000)',
+                  type='int',
+                  default=25000,
+                  action='store')
+(options, args) = parser.parse_args()
+
+style = args[0]
+output = args[1]
+
 SRS = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+stylefile = os.path.join('styles', style, '%s.xml' % style)
+extension = 'png'
+output_tif = '%s.tif' % output
+pixels_per_m = 100.0 / 0.028  # 1 pixel = 0.28mm
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
-style = 'osm'
-stylefile = os.path.join('styles', style, '%s.xml' % style)
-output = 'output.png'
-extension = output.rsplit('.', 1)[-1]
-output_tif = output.replace(extension, 'tif')
-width = 8000
-height = 8000
-extent = (2.04, 43.88, 2.22, 43.98)
+logging.basicConfig(level=logging.INFO, format="\033[92m%(message)s\033[0m")
 
 
 def main():
-    map = Map(width, height)
-    load_map(map, stylefile)
-
     wgs84 = Projection('+proj=latlong +datum=WGS84')
     projection = Projection(SRS)
     env = ProjTransform(wgs84, projection)
-    bbox = env.forward(Box2d(*extent))
+    bbox = env.forward(Box2d(*options.extent))
+
+    width_m = bbox.width() / options.scale
+    height_m = bbox.height() / options.scale
+    width_pix = int(math.ceil(pixels_per_m * width_m))
+    height_pix = int(math.ceil(pixels_per_m * height_m))
+
+    map = Map(width_pix, height_pix)
+    load_map(map, stylefile)
+    map.srs = SRS
     map.zoom_to_box(bbox)
 
+    logger.info('Extent size: %sm x %sm' % (bbox.width(), bbox.height()))
+    logger.info('Scale: 1:%s' % options.scale)
+    logger.info('Scaled size: %sm x %sm' % (width_m, height_m))
+    logger.info('Image size: %s x %s' % (width_pix, height_pix))
     logger.info('Map extent: %s' % map.envelope())
     logger.info('Map center: %s' % map.envelope().center())
     logger.info('At current scale of %s...' % map.scale())
